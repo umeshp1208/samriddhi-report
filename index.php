93bdaaf3704be5e0696a3a88bca1dbd1ece3568c
Umesh P <umesh.prajapati@bytestechnolab.com>
2025-01-06 14:39:58 +0530

    Add few datatable cdns

--- index.php
+++ index.php
@@ -251,6 +251,11 @@ $baseCopyDate = isset($_POST['base_date']) ? $_POST['base_date'] : '2024-12-10';
     <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.3.1/js/dataTables.buttons.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.3.1/js/buttons.html5.min.js"></script>
+
+    <script type="text/javascript" src="https://cdn.datatables.net/buttons/3.2.0/js/dataTables.buttons.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/buttons/3.2.0/js/buttons.dataTables.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/buttons/3.2.0/js/buttons.html5.min.js"></script>
     <script>
         $(function() {
             $("#date").datepicker({
